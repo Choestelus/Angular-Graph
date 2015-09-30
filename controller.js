17941dfe1ed00a89f0e3ed47b303ac97837fd0d7
Choestelus Supershibe <sprintf.null@gmail.com>
2015-09-30 16:07:49 +0700

    IOPS and disktop graph

--- controller.js
+++ controller.js
@@ -5,14 +5,29 @@ function Hello($scope, $http) {
         // success(function(data) {
         //    $scope.greeting = data;
         // });
-        $scope.greeting = {
-            "labels" : ["1", "2", "3", "4", "5"],
-            "series" : ["Series 1", "Series 2"],
-            "data" : [
-                [65, 59, 80, 81, 56, 55, 40],
-                [28, 48, 40, 19, 86, 27, 90]
-            ],
-            "colors" : [ "#97BBCD", "#DCDCDC", "#F7464A", "#46BFBD", "#FDB45C", "#949FB1", "#4D5360" ]
 
+        $scope.disktop = {
+            "labels" : ["mysqld", "sleep", "gitlab-unicorn-", "stapio"],
+            "series" : ["Read", "Write"],
+            "data" : [
+                [15843328, 1224, 788, 0],
+                [5071360, 0, 0, 562]
+            ]
+
+        }
+
+        $scope.iosize = {
+
+        }
+
+        $scope.iops = {
+            "labels" : ["mysqld", "postmark", "top", "postgress", "pgrep", "redis-server", "ceph-mds",
+                        "sshd", "gitlab-unicorn-", "irqbalance", "ruby", "crond", "awk", "docker", "ksmtuned",
+                        "python", "java", "tpcc_start", "tmux"],
+            "series" : ["read", "write"],
+            "data" : [
+                [10456, 37821, 14636, 153, 1048, 990, 144, 68, 172, 336, 98, 84, 10, 24, 85, 42, 12, 6, 0, 18],
+                [197, 54081, 55, 30, 0, 133, 0, 67, 0, 0, 2, 157, 0, 4, 0, 2, 0, 0, 9, 0]
+            ]
         }
 }
